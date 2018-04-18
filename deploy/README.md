a955b283f8e1a23e2b06254004ec3c39b2cc7ccf
Alex <gitHub@fr3k4life.de>
2018-04-18 11:18:07 +0200

    added deployment folder

---
+++ deploy/README.md
@@ -0,0 +1,10 @@
+# Repository for Deployment via Gitlab
+## :shopware
+* Is based on node:alpine
+* includes needed shopware version via ENV
+* lints .less and .js
+    * via lessc and jshint
+
+## :rsync
+* simple debian:jessie-slim
+* used for deployment via rsync
---
+++ deploy/shopware/inc/shopwareDownload.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+version=$1
+case "$1" in
+        latest)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.3.4_05cdb056b32446d2bb555aa00f85aac06ec72060.zip -O shopware.zip
+            ;;
+
+        5.3.4)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.3.4_05cdb056b32446d2bb555aa00f85aac06ec72060.zip -O shopware.zip
+            ;;
+
+        5.3.3)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.3.3_0e50204087219ada9cdd9a74cd17cbb264e8c0a4.zip -O shopware.zip
+            ;;
+
+        5.3.2)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.3.2_5ab7c48a261445eaeb25273cb34c1a6ae3102741.zip -O shopware.zip
+            ;;
+
+        5.3.1)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.3.1_e42b3bfc041e6115ae04948dccbd1cd91d5c0564.zip -O shopware.zip
+            ;;
+
+        5.3.0)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.3.0_51914cdfbd690d51f3dbe4049705b033ff98811f.zip -O shopware.zip
+            ;;
+
+        5.2.27)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.27_56d5aabc56c2e48d84084d0381a72a3897d5263f.zip -O shopware.zip
+            ;;
+
+        5.2.26)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.26_2437513ecb3e6d0340bab707cf4b680666d38ed5.zip -O shopware.zip
+            ;;
+
+        5.2.25)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.25_54d4a3c78efa09f63fc71077537ea409696dfeca.zip -O shopware.zip
+            ;;
+
+        5.2.24)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.24_2056ea7f0d0363e0d4c0678a4a68fc35db547965.zip -O shopware.zip
+            ;;
+
+        5.2.23)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.23_21d35d62918428c401b967c69fe73ff38a1f00a2.zip -O shopware.zip
+            ;;
+
+        5.2.22)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.22_0010210a2d8f7c275ca9bbed06b0f213cbb4b048.zip -O shopware.zip
+            ;;
+
+        5.2.21)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.21_2164cff64b62f5d7aa68c0ae8a231ce750430e65.zip -O shopware.zip
+            ;;
+
+        5.2.20)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.20_99580d1ac8d442f18721399a14e042ea610fd1a0.zip -O shopware.zip
+            ;;
+
+        5.2.19)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.19_4db5d68242f44778ceee816f958e6ec9dfa731b8.zip -O shopware.zip
+            ;;
+
+        5.2.18)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.18_40832027120b85c9620e6651b4a9b32290e10a22.zip -O shopware.zip
+            ;;
+
+        5.2.17)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.17_582842251fc465c404e74b622e2638233d353d71.zip -O shopware.zip
+            ;;
+
+        5.2.16)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.16_361b837b2dba5bbc5fb7064a718ebf57cfac5cad.zip -O shopware.zip
+            ;;
+
+        5.2.15)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.15_f78c4fd1b3d68d7c3542a7aad2ac33d90726f8db.zip -O shopware.zip
+            ;;
+
+        5.2.14)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.14_8ead8335c085d4677a4e7ad7d3399b094c1bb26e.zip -O shopware.zip
+            ;;
+
+        5.2.13)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.13_dbca3b24f22f8f52512975133374b9b85a39a123.zip -O shopware.zip
+            ;;
+
+        5.2.12)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.12_8c8f67d848ef15aafaac6fed5f0eef062644250f.zip -O shopware.zip
+            ;;
+
+        5.2.11)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.11_c2c89cba1e16f554ff7239574a2865254a6b97a2.zip -O shopware.zip
+            ;;
+
+        5.2.10)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.10_81bb8855627da325b797a477602189377b411bcf.zip -O shopware.zip
+            ;;
+
+        5.2.9)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.9_29c0a379c74f8fd89911f31dd671a0c7d28f18fe.zip -O shopware.zip
+            ;;
+
+        5.2.8)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.8_b41bb9f24d00b3d8a0bf880361ec06fc5e2e67d5.zip -O shopware.zip
+            ;;
+
+        5.2.7)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.7_b2081fd410306d74001ae1a2ce725b003c3e0259.zip -O shopware.zip
+            ;;
+
+        5.2.6)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.6_ecdfff604b1fa93a3b50606d378f9e2bf681890f.zip -O shopware.zip
+            ;;
+
+        5.2.5)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.5_cc3b6e432af2144fdd4c5e8e1b28cda0398ef7f4.zip -O shopware.zip
+            ;;
+
+        5.2.4)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.4_b1a52d04c9c8cd60205c181eb7d51aa5a516bff0.zip -O shopware.zip
+            ;;
+
+        5.2.3)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.3_3066bee006dc8fa082b6691a6af186810b3e4f05.zip -O shopware.zip
+            ;;
+
+        5.2.2)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.2_fec9a405e4d6043625058bc5bf3badecd9197333.zip -O shopware.zip
+            ;;
+
+        5.2.1)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.1_b87844ddbd0b61f85b78764983daf220662fd273.zip -O shopware.zip
+            ;;
+
+        5.2.0)
+            wget -q http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.0_b9d72b7ea4f2ad7092d761ebb4f38373f2b3ff43.zip -O shopware.zip
+            ;;
+
+        *)
+            exit 1
+esac
+unzip -qq shopware.zip 'themes/Frontend/Bare/frontend/_public/*' -d ./includes/
+unzip -qq shopware.zip 'themes/Frontend/Responsive/frontend/_public/*' -d ./includes/
+rm -rf shopware.zip
